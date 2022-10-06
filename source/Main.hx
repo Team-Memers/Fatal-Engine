725805fd9ccc822e25806160bd9c8db759807fc5
Goofgoof43 <amiausername@gmail.com>
2022-10-06 19:56:35 -0400

    testing purposes (pun intended)

--- source/Main.hx
+++ source/Main.hx
@@ -54,7 +54,7 @@ class Main extends Sprite
 		[ [songs to use], [characters in songs], [color of week], name of week ]
 	**/
 	public static var gameWeeks:Array<Dynamic> = [
-		[['Tutorial'], ['gf'], [FlxColor.fromRGB(129, 100, 223)], 'Tutorial - Tutorial'],
+		[['Test'], ['gf'], [FlxColor.fromRGB(129, 100, 223)], 'Tutorial - Tutorial'],
 		[
 			['Bopeebo', 'Fresh', 'Dadbattle'],
 			['dad', 'dad', 'dad'],
