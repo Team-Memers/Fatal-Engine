6f88728ba9d5da5ed2cfb94811a5202fdbf040cd
Ty_1991 <67247243+Ty1991twitter@users.noreply.github.com>
2022-10-06 08:42:27 -0400

    Update TitleState.hx

--- source/meta/state/TitleState.hx
+++ source/meta/state/TitleState.hx
@@ -52,6 +52,10 @@ class TitleState extends MusicBeatState
 
 	override public function create():Void
 	{
+		#if android
+		FlxG.android.preventDefaultKeys = [BACK];
+		#end
+			
 		controls.setKeyboardScheme(None, false);
 		curWacky = FlxG.random.getObject(getIntroTextShit());
 		super.create();
