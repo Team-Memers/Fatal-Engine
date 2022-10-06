97f51eec6288cbd47eb595abc3ad4aa068dac9ea
Ty_1991 <67247243+Ty1991twitter@users.noreply.github.com>
2022-10-06 08:39:05 -0400

    Update Discord.hx

--- source/meta/data/dependency/Discord.hx
+++ source/meta/data/dependency/Discord.hx
@@ -1,6 +1,6 @@
 package meta.data.dependency;
 
-#if DISCORD_RPC
+#if desktop
 import discord_rpc.DiscordRpc;
 #end
 import lime.app.Application;
@@ -11,7 +11,7 @@ import lime.app.Application;
 **/
 class Discord
 {
-	#if DISCORD_RPC
+	#if desktop
 	// set up the rich presence initially
 	public static function initializeRPC()
 	{
@@ -24,17 +24,20 @@ class Discord
 
 		// THANK YOU GEDE
 		Application.current.window.onClose.add(shutdownRPC);
+		#end
 	}
 
 	// from the base game
 	static function onReady()
 	{
+		#if desktop
 		DiscordRpc.presence({
 			details: "",
 			state: null,
 			largeImageKey: 'iconog',
 			largeImageText: "Forever Engine"
 		});
+		#end
 	}
 
 	static function onError(_code:Int, _message:String)
@@ -51,6 +54,7 @@ class Discord
 
 	public static function changePresence(details:String = '', state:Null<String> = '', ?smallImageKey:String, ?hasStartTimestamp:Bool, ?endTimestamp:Float)
 	{
+		#if desktop
 		var startTimestamp:Float = (hasStartTimestamp) ? Date.now().getTime() : 0;
 
 		if (endTimestamp > 0)
@@ -66,14 +70,15 @@ class Discord
 			startTimestamp: Std.int(startTimestamp / 1000),
 			endTimestamp: Std.int(endTimestamp / 1000)
 		});
-
+                #end
 		// trace('Discord RPC Updated. Arguments: $details, $state, $smallImageKey, $hasStartTimestamp, $endTimestamp');
 	}
 
 	public static function shutdownRPC()
 	{
+		#if desktop
 		// borrowed from izzy engine -- somewhat, at least
 		DiscordRpc.shutdown();
+		#end
 	}
-	#end
 }
